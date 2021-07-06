6c8d7a26ad4f950343ae60e631124e9890d2891a
Denisse-Arreola <arxie.denisse@gmail.com>
2021-07-06 10:51:01 -0700

    TCS3200 Implemented

--- nodeMCU/esp_publisher.ino
+++ nodeMCU/esp_publisher.ino
@@ -12,6 +12,13 @@
 #define DHTTYPE DHT11             // DHT 11
 DHT dht(DHTPIN, DHTTYPE);         // Initialize DHT
 
+// PENDING
+/*
+// Define Ultrasonic Sensor
+#define TRIG_PIN 12                               // D14
+#define ECHO_PIN 13                               // D13
+*/
+
 // HX711 circuit wiring
 #define LOADCELL_DOUT_PIN 18          // D18
 #define LOADCELL_SCK_PIN 19           // D19
@@ -19,12 +26,12 @@ DHT dht(DHTPIN, DHTTYPE);         // Initialize DHT
 HX711 scale;
 
 // Define TCS3200
-/* PENDING
 #define S0 4
 #define S1 5
-#define S2 6
-#define S3 7
-#define sensorOut 8*/
+#define S2 2
+#define S3 21
+#define sensorOut 22
+// int frequency = 0;
 
 // Network credentials
 const char *ssid = "INFINITUM67F2_2.4";
@@ -105,6 +112,19 @@ void setup() {
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);   // Initialize HX711
   scale.set_scale(CALIBRATION_FACTOR);                // Set the scale on based the calibration factor
   scale.tare();                                       // Assuming there is no weight on the scale at start up, reset the scale to 0
+  
+  pinMode(S0, OUTPUT);                                // 
+  pinMode(S1, OUTPUT);                                //
+  pinMode(S2, OUTPUT);                                // Initialize TCS3200 pins
+  pinMode(S3, OUTPUT);                                //
+  pinMode(sensorOut, INPUT);                          //
+                                                      
+  digitalWrite(S0,HIGH);                              //
+  digitalWrite(S1,HIGH);                              // Setting frequency-scaling to 100% for TCS3200
+
+  //pinMode(TRIG_PIN, OUTPUT);                          //
+  //pinMode(ECHO_PIN, INPUT);                           // Initialize Ultrasonic Sensor Pins
+  
   setup_wifi();                                       // Connecting to the network
   client.setServer(mqtt_broker, 1883);                // Initializing connection with the broker
   client.setCallback(callback);                       // Callback based on the function with the same name
@@ -114,34 +134,63 @@ void loop() {
   if(!client.connected()){
     reconnect();  
   }
-
+  
   client.loop();
   unsigned long now = millis();
   if(now - lastMsg > Ts){
     float ToC = dht.readTemperature();
     float RH = dht.readHumidity();
     float weight = scale.get_units();       // This part is not calibrated yet
+
+    // assign variables for the color
+    // setting red filtered photodiodes to be read
+    digitalWrite(S2,LOW);
+    digitalWrite(S3,LOW);
+    // Reading the output frequency
+    int R = pulseIn(sensorOut, LOW);
+    // Setting Green filtered photodiodes to be read
+    digitalWrite(S2,HIGH);
+    digitalWrite(S3,HIGH);
+    // Reading the output frequency
+    int G = pulseIn(sensorOut, LOW);
+    // Setting Blue filtered photodiodes to be read
+    digitalWrite(S2,LOW);
+    digitalWrite(S3,HIGH);
+    // Reading the output frequency
+    int B = pulseIn(sensorOut, LOW);
    
     if (isnan(ToC) || isnan(RH)) {
       Serial.println(F("Failed to read from DHT sensor!"));
     } else if(scale.is_ready()){
-      Serial.println("HX711 not found.");
+      Serial.println(F("HX711 not found."));
+    }else if (isnan(R) || isnan(G) || isnan(B)){
+      Serial.println(F("Failed to read from TCS3200"));
     } else {
+      
+      // JSON message values
       String JSON_msg;         
-      StaticJsonDocument<300> doc; 
-      doc["n"] = "0319124849"; 
-      doc["ToC"] = ToC;
-      doc["RH"] = RH;
-      doc["W"] = weight;
-      serializeJson(doc, JSON_msg);
+      StaticJsonDocument<300> values; 
+      values["n"] = "0319124849"; 
+      values["ToC"] = ToC;
+      values["RH"] = RH;
+      values["W"] = weight;
+
+      // JSON object for the color inside "values"
+      JsonObject color = values.createNestedObject("Color");
+      color["R"] = R;
+      color["G"] = G;
+      color["B"] = B;
+
+      // Serialize values into JSON_msg
+      serializeJson(values, JSON_msg);
       
       Serial.print(F("Publishing message: "));
       Serial.println(JSON_msg);
       
       // MQTT Publish to topic
-      char JSON_msg_array[60];
+      char JSON_msg_array[100];
       char JSON_msg_length = JSON_msg.length();
-      JSON_msg.toCharArray(JSON_msg_array, 60);
+      JSON_msg.toCharArray(JSON_msg_array, 100);
       Serial.println(JSON_msg_array);
       if(client.connected()){
         client.publish(OutTopic, JSON_msg_array);
