5247b03cf63522201bc93626494761fa12c644eb
Denisse-Arreola <72837771+Denisse-Arreola@users.noreply.github.com>
2021-07-08 20:36:49 -0700

    GreenFresh app classes changes

--- androidApplication/app/src/main/java/utt/student/greenfresh/classes/Sensor.java
+++ androidApplication/app/src/main/java/utt/student/greenfresh/classes/Sensor.java
@@ -6,30 +6,26 @@ public class Sensor {
     //attributes
     private String name;
     private Drawable icon;
-
-
-
-    private  double value;
+    private String value;
 
     //getters and setters
-
     public String getName(){ return  name; }
     public void  setName(String name){ this.name = name; }
 
     public Drawable getIcon(){ return  icon; }
     public void  setIcon(Drawable icon){ this.icon = icon; }
 
-    public void setValue(double value) { this.value = value; }
-    public double getValue() { return value; }
+    public void setValue(String value) { this.value = value; }
+    public String getValue() { return value; }
 
     //constructor
     public void Sensor(){
         this.name = "";
         this.icon = null;
-        this.value = 0.0;
+        this.value = "";
     }
 
-    public Sensor (String name, Drawable icon, double value){
+    public Sensor(String name, Drawable icon, String value){
         this.name = name;
         this.icon = icon;
         this.value = value;
