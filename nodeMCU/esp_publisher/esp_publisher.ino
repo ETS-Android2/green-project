a1e2b163a4d8a18466760d328cc0cc6452ec950d
Denisse-Arreola <72837771+Denisse-Arreola@users.noreply.github.com>
2021-07-09 16:34:22 -0700

    ESP sketch modified - Topics and New functions

--- nodeMCU/esp_publisher/esp_publisher.ino
+++ nodeMCU/esp_publisher/esp_publisher.ino
@@ -12,12 +12,9 @@
 #define DHTTYPE DHT11             // DHT 11
 DHT dht(DHTPIN, DHTTYPE);         // Initialize DHT
 
-// PENDING
-/*
 // Define Ultrasonic Sensor
-#define TRIG_PIN 12                               // D14
-#define ECHO_PIN 13                               // D13
-*/
+#define TRIG_PIN 32                               // D32
+#define ECHO_PIN 33                               // D33
 
 // HX711 circuit wiring
 #define LOADCELL_DOUT_PIN 18          // D18
@@ -36,14 +33,24 @@ HX711 scale;
 // Network credentials
 const char *ssid = "INFINITUM67F2_2.4";
 const char *password = "Ma2F3YqgAb"; 
-const char *mqtt_broker = "broker.hivemq.com";   // For testing
+const char *mqtt_broker = "201.171.7.12";
+// const char *mqtt_broker = "broker.hivemq.com";   // For testing
+
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 unsigned long lastMsg = 0;
-char OutTopic[] = "/UTT/0319124849/";
-char InTopic[] = "/UTT/0319124849/cntrl/";
-int Ts = 5000;    // miliseconds sampling rate
+
+// Topics
+char publishProductionLine[] = "/UTT/register/productionLine";
+char publishFruitTopic[] = "/UTT/register/fruitReadings";
+char publishAreaTopic[] = "/UTT/register/enviromentVariables";
+char subscribedTopic[] = "/UTT/register/readingsValidation";
+
+// miliseconds sampling rate for the area variables
+int samplingRateA = 60000;    
+// miliseconds sampling rate for fruit results
+int samplingRateF = 5000;    
 
 void setup_wifi(){
   delay(10);
@@ -95,7 +102,7 @@ void reconnect(){
     // Attempt to connect
     if(client.connect(clientId.c_str())){
       Serial.println("connected");  
-      client.subscribe(InTopic);
+      client.subscribe(subscribedTopic);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -105,74 +112,35 @@ void reconnect(){
   }
 }
 
-void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);                       // Initialize the BUILTIN_LED pin as an output
-  Serial.begin(74880);                                // Initializing on 748800
-  dht.begin();                                        // Initialize dht
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);   // Initialize HX711
-  scale.set_scale(CALIBRATION_FACTOR);                // Set the scale on based the calibration factor
-  scale.tare();                                       // Assuming there is no weight on the scale at start up, reset the scale to 0
-  
-  pinMode(S0, OUTPUT);                                // 
-  pinMode(S1, OUTPUT);                                //
-  pinMode(S2, OUTPUT);                                // Initialize TCS3200 pins
-  pinMode(S3, OUTPUT);                                //
-  pinMode(sensorOut, INPUT);                          //
-                                                      
-  digitalWrite(S0,HIGH);                              //
-  digitalWrite(S1,HIGH);                              // Setting frequency-scaling to 100% for TCS3200
+void sendFruitResults(){
+  float weight = scale.get_units();       // This part is not calibrated yet
 
-  //pinMode(TRIG_PIN, OUTPUT);                          //
-  //pinMode(ECHO_PIN, INPUT);                           // Initialize Ultrasonic Sensor Pins
-  
-  setup_wifi();                                       // Connecting to the network
-  client.setServer(mqtt_broker, 1883);                // Initializing connection with the broker
-  client.setCallback(callback);                       // Callback based on the function with the same name
-}
+  // assign variables for the color
+  // setting red filtered photodiodes to be read
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,LOW);
+  // Reading the output frequency
+  int R = pulseIn(sensorOut, LOW);
+  // Setting Green filtered photodiodes to be read
+  digitalWrite(S2,HIGH);
+  digitalWrite(S3,HIGH);
+  // Reading the output frequency
+  int G = pulseIn(sensorOut, LOW);
+  // Setting Blue filtered photodiodes to be read
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,HIGH);
+  // Reading the output frequency
+  int B = pulseIn(sensorOut, LOW);
 
-void loop() {
-  if(!client.connected()){
-    reconnect();  
-  }
-  
-  client.loop();
-  unsigned long now = millis();
-  if(now - lastMsg > Ts){
-    float ToC = dht.readTemperature();
-    float RH = dht.readHumidity();
-    float weight = scale.get_units();       // This part is not calibrated yet
-
-    // assign variables for the color
-    // setting red filtered photodiodes to be read
-    digitalWrite(S2,LOW);
-    digitalWrite(S3,LOW);
-    // Reading the output frequency
-    int R = pulseIn(sensorOut, LOW);
-    // Setting Green filtered photodiodes to be read
-    digitalWrite(S2,HIGH);
-    digitalWrite(S3,HIGH);
-    // Reading the output frequency
-    int G = pulseIn(sensorOut, LOW);
-    // Setting Blue filtered photodiodes to be read
-    digitalWrite(S2,LOW);
-    digitalWrite(S3,HIGH);
-    // Reading the output frequency
-    int B = pulseIn(sensorOut, LOW);
-   
-    if (isnan(ToC) || isnan(RH)) {
-      Serial.println(F("Failed to read from DHT sensor!"));
-    } else if(scale.is_ready()){
-      Serial.println(F("HX711 not found."));
-    }else if (isnan(R) || isnan(G) || isnan(B)){
-      Serial.println(F("Failed to read from TCS3200"));
-    } else {
-      
-      // JSON message values
+  if(scale.is_ready()){
+    Serial.println(F("HX711 not found."));
+  }else if (isnan(R) || isnan(G) || isnan(B)){
+    Serial.println(F("Failed to read from TCS3200"));
+  } else {
+    // JSON message values
       String JSON_msg;         
       StaticJsonDocument<300> values; 
       values["n"] = "0319124849"; 
-      values["ToC"] = ToC;
-      values["RH"] = RH;
       values["W"] = weight;
 
       // JSON object for the color inside "values"
@@ -193,14 +161,113 @@ void loop() {
       JSON_msg.toCharArray(JSON_msg_array, 100);
       Serial.println(JSON_msg_array);
       if(client.connected()){
-        client.publish(OutTopic, JSON_msg_array);
+        client.publish(publishFruitTopic, JSON_msg_array);
         Serial.print("Published to topic:");
-        Serial.println(OutTopic);
+        Serial.println(publishFruitTopic);
+        Serial.println();
       } else {
         Serial.print("Not connected to broker... couldn't send MQTT message...");  
+        Serial.println();
       }
-    }
-    
-    lastMsg = millis();
   }
+  
+}
+
+void sendAreaVariables(){
+  float ToC = dht.readTemperature();
+  float RH = dht.readHumidity();
+
+  if (isnan(ToC) || isnan(RH)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+  }else{
+    // JSON message values
+    String JSON_msg;         
+    StaticJsonDocument<300> values; 
+    values["n"] = "0319124849"; 
+    values["ToC"] = ToC;
+    values["RH"] = RH;
+
+    // Serialize values into JSON_msg
+    serializeJson(values, JSON_msg);
+      
+    Serial.print(F("Publishing message: "));
+    Serial.println(JSON_msg);
+      
+    // MQTT Publish to topic
+    char JSON_msg_array[100];
+    char JSON_msg_length = JSON_msg.length();
+    JSON_msg.toCharArray(JSON_msg_array, 100);
+    Serial.println(JSON_msg_array);
+    if(client.connected()){
+      client.publish(publishAreaTopic, JSON_msg_array);
+      Serial.print("Published to topic:");
+      Serial.println(publishAreaTopic);
+      Serial.println();
+    } else {
+      Serial.print("Not connected to broker... couldn't send MQTT message..."); 
+      Serial.println(); 
+    }
+  }  
+}
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);                       // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(74880);                                // Initializing on 748800
+  dht.begin();                                        // Initialize dht
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);   // Initialize HX711
+  scale.set_scale(CALIBRATION_FACTOR);                // Set the scale on based the calibration factor
+  scale.tare();                                       // Assuming there is no weight on the scale at start up, reset the scale to 0
+  
+  pinMode(S0, OUTPUT);                                // 
+  pinMode(S1, OUTPUT);                                //
+  pinMode(S2, OUTPUT);                                // Initialize TCS3200 pins
+  pinMode(S3, OUTPUT);                                //
+  pinMode(sensorOut, INPUT);                          //
+                                                      
+  digitalWrite(S0,HIGH);                              //
+  digitalWrite(S1,HIGH);                              // Setting frequency-scaling to 100% for TCS3200
+
+  pinMode(TRIG_PIN, OUTPUT);                          //
+  pinMode(ECHO_PIN, INPUT);                           // Initialize Ultrasonic Sensor Pins
+  
+  setup_wifi();                                       // Connecting to the network
+  //client.setServer(mqtt_broker, 1883);                // Initializing connection with the broker
+  client.setServer(mqtt_broker, 6000);                // Initializing connection with the broker
+  client.setCallback(callback);                       // Callback based on the function with the same name
+}
+
+void loop() {
+  if(!client.connected()){
+    reconnect();  
+  }
+
+  // Verify if any fruit is on the scale
+  // using the ultrasonic sensor
+  float duration, distance, scale;         
+  // Declaring a duration and distance for the process
+  // and scale depeding the heigth of the scale
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  duration = pulseIn(ECHO_PIN, HIGH);
+  distance = (duration / 2) * 0.0344;
+  scale = 10;
+
+  client.loop();
+  unsigned long now = millis();
+
+  if((distance <= scale && distance > 0) && (now - lastMsg > samplingRateF)){
+    Serial.println("\nFruit detected \nInitialize Readings...");
+    sendFruitResults();
+  }
+
+  now = millis();
+  if(now - lastMsg > samplingRateA){
+    sendAreaVariables();
+  }
+
+  lastMsg = millis();
+ 
 }
