c6ce4f3d8898e2a0484e4e82b33bc2088f8dfa72
Tonatiu010 <l.tonatiuh010@gmail.com>
2021-07-14 12:22:10 -0700

    Icons addes

---
+++ angular-website/green-fresh/src/app/components/production-line/production-line.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductionLineComponent } from './production-line.component';
+
+describe('ProductionLineComponent', () => {
+  let component: ProductionLineComponent;
+  let fixture: ComponentFixture<ProductionLineComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProductionLineComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductionLineComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular-website/green-fresh/src/app/components/production-line/production-line.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import '@fortawesome/free-solid-svg-icons';
+
+
+@Component({
+  selector: 'app-production-line',
+  templateUrl: './production-line.component.html',
+  styleUrls: ['./production-line.component.css']
+})
+export class ProductionLineComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
