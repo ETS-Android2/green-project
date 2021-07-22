d01404905cac00d29fc709e4d287882ebe8740f6
danielgrcXO <danielitoelguarneritos@gmail.com>
2021-07-22 12:52:14 -0700

    Garcia Guarneros Instructivo Api (en progreso)

--- api/logs/errorLogs.py
+++ api/logs/errorLogs.py
@@ -3,9 +3,11 @@ import logging
 
 class ErrorLogs:
 
-        logging.basicConfig(filename='logs/errorLogs.log', filemode ='a',
-        format='%(asctime) s %(name)s - %(levelname)s - %(message)s')
-        logging.error("** LOG ERROR: The MySQL connection has failed:")
+        def __init__(self,message):
+                logging.basicConfig(filename='logs/errorLogs.log', filemode ='a',
+                format='%(asctime) s %(name)s - %(levelname)s - %(message)s')
+                logging.error(message)
+                print(message)
 
 
 
