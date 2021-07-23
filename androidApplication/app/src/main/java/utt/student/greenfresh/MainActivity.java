a4cc614022d25a0a5206ff7e5091d8b507184f52
Tonatiu010 <l.tonatiuh010@gmail.com>
2021-07-22 23:03:23 -0700

    22 jul 2021

--- androidApplication/app/src/main/java/utt/student/greenfresh/MainActivity.java
+++ androidApplication/app/src/main/java/utt/student/greenfresh/MainActivity.java
@@ -30,7 +30,7 @@ import utt.student.greenfresh.classes.Status;
 public class MainActivity extends AppCompatActivity {
     // variables
     private RequestQueue queue;
-    private static String baseURL = "http://189.223.79.36:7000/";
+    private static String baseURL = "http://192.168.1.65:5000/";
     // array List
     private ArrayList<Fruit> fruits = new ArrayList<>();
     private ArrayList<FruitReadings> fruitReadings = new ArrayList<>();
